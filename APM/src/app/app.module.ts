b8f55a96a7a4737f5abde8ede6f22eda2f315a8a
asilvama <asilvam1@yahoo.com>
2018-01-12 18:25:25 -0500

    ultimo video en PS

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { WelcomeComponent } from './home/welcome.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -23,7 +25,14 @@ import { ProductDetailComponent } from './products/product-detail.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id', component: ProductListComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
